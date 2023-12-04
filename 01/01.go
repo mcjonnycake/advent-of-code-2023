1c5ea058bcfaec3c52fec6828cf380dac0ad841e
mcjonnycake <20602012+mcjonnycake@users.noreply.github.com>
2023-12-03 21:50:42 -0600

    Tests from the problem doesn't cover case like 'oneight' which means that the problem is incorrect as it currently stands. Saving for tomorrow after work, gonna try and solve #2 before going to bed

--- 01/01.go
+++ 01/01.go
@@ -63,16 +63,39 @@ func get_first_last_digits_with_text(input_text string) (int, int) {
 }
 
 func replace_text_numbers(input_text string) string {
-	input_text = strings.ReplaceAll(input_text, "zero", "0")
-	input_text = strings.ReplaceAll(input_text, "one", "1")
-	input_text = strings.ReplaceAll(input_text, "two", "2")
-	input_text = strings.ReplaceAll(input_text, "three", "3")
-	input_text = strings.ReplaceAll(input_text, "four", "4")
-	input_text = strings.ReplaceAll(input_text, "five", "5")
-	input_text = strings.ReplaceAll(input_text, "six", "6")
-	input_text = strings.ReplaceAll(input_text, "seven", "7")
-	input_text = strings.ReplaceAll(input_text, "eight", "8")
-	input_text = strings.ReplaceAll(input_text, "nine", "9")
+	var lowest_idx int = -1
+	var elem_idx int = -1
+	var lowest_idx_num = ""
+	var lowest_idx_text = ""
+	var found_num bool = false
+
+	num_text_arr := [10]string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+	for {
+		for index, element := range num_text_arr { 
+			if strings.Contains(input_text, element) {
+				elem_idx = strings.Index(input_text, element)
+				if lowest_idx == -1 {
+					found_num = true
+					lowest_idx = elem_idx
+					lowest_idx_num = strconv.Itoa(index)
+					lowest_idx_text = element
+				} else if lowest_idx > elem_idx {
+					lowest_idx = elem_idx
+					lowest_idx_num = strconv.Itoa(index)
+					lowest_idx_text = element
+				}
+			}
+		}
+
+		if found_num {
+			input_text = strings.Replace(input_text, lowest_idx_text, lowest_idx_num, 1)
+			found_num = false
+			lowest_idx = -1
+		} else {
+			break
+		}
+	}
 
 	return input_text
 }
@@ -114,17 +137,39 @@ func run_part_2_test(text_input string) {
 	fmt.Printf("%s, %d\n", text_input, calibration_number)
 }
 
-func main() {
-	/*
-		fmt.Println("Part 1 Testing")
-		run_part_1_test(test_1_1)
-		run_part_1_test(test_1_2)
-		run_part_1_test(test_1_3)
-		run_part_1_test(test_1_4)
+func run_part_2() {
+	content, err := ioutil.ReadFile(test_file)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-		fmt.Println()
-		run_part_1()
-	*/
+	input_text := string(content)
+
+	var running_total int = 0
+
+	scanner := bufio.NewScanner(strings.NewReader(input_text))
+	for scanner.Scan() {
+		textLine := scanner.Text()
+		first_digit, last_digit := get_first_last_digits_with_text(textLine)
+		calibration_number := create_calibration_number(first_digit, last_digit)
+
+		fmt.Printf("Text Input: %s - Calibration Number: %d\n", textLine, calibration_number)
+
+		running_total += calibration_number
+	}
+	fmt.Printf("Part 2: %d\n", running_total)
+}
+
+func main() {
+	fmt.Println("Part 1 Testing")
+	run_part_1_test(test_1_1)
+	run_part_1_test(test_1_2)
+	run_part_1_test(test_1_3)
+	run_part_1_test(test_1_4)
+
+	fmt.Println()
+	run_part_1()
+	fmt.Println()
 
 	fmt.Println("Part 2 Testing")
 	run_part_2_test(test_2_1)
@@ -134,4 +179,9 @@ func main() {
 	run_part_2_test(test_2_5)
 	run_part_2_test(test_2_6)
 	run_part_2_test(test_2_7)
+
+	fmt.Println()
+	run_part_2()
+
+	
 }
